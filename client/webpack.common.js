f14d9694afffbb39fa7f7b0867be796ecc2b8cca
Mohd-Mahin <mahinit3016@gmail.com>
2020-07-31 22:43:33 +0530

    dfdsf

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -8,6 +8,18 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -14,16 +14,4 @@ module.exports = merge(common, {
       },
     },
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
 });
