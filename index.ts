0ec5c6ceebfebce49c82ea7219eb28b6143f2390
Mohd-Mahin <mahinit3016@gmail.com>
2020-08-09 23:06:41 +0530

    minor change

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { getApiRouter } from "./server/controller/api-router";
 import path from "path";
+import { homepage } from "./home-page";
 import { notFound } from "./server/utils/helper-func";
 
 const PORT = process.env.PORT || 14000;
@@ -10,18 +11,7 @@ async function main() {
     .use(express.json())
     .use(express.static(path.join(__dirname, "..", "client", "dist")))
     .use("/api", getApiRouter())
-    // // .use(notFound)
-    .get("*", (req, res) => {
-      res.send(`
-      <!doctype html>
-      <html lang="en">
-      <head>
-      <meta charset="UTF-8"/><meta name="viewport" content="width=<device-width>,initial-scale=1"/>
-      <title>Ecommerce 1.x</title><style>body {
-        margin: 0;
-      }</style></head><body><div id="root"></div><script src="/bundle.js"></script></body></html>
-      `);
-    })
+    .get("*", homepage)
     .listen(PORT, () =>
       console.log(`Server listening on http://localhost:${PORT}`)
     );
